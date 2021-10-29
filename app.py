ed37eb24e00f38a680d05757a922f13b40c7fb4d
Kirralex <kirranalex@outlook.com>
2021-10-29 21:57:53 +0300

    Add path_configs and more backend

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+import yaml
+import cv2
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    return render_template('main_page.html')
+    # image_classes = list(2)
+
+    with open('images_paths.yml') as f:
+        images_paths = yaml.load(f, Loader=yaml.FullLoader)
+    images_classes = ['safe', 'dangerous']
+
+    return render_template('main_page.html', images_paths=images_paths, image_classes=images_classes)
 
 
 if __name__ == '__main__':
