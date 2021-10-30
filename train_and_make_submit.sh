23f656ca20fe09b1c0f30d329ec7301249a99158
Anvar <ankhafizov1998@yandex.ru>
2021-10-30 21:50:47 +0300

    epochs moved to parans

--- train_and_make_submit.sh
+++ train_and_make_submit.sh
@@ -5,6 +5,7 @@ dataset_in_yolo_form_location=yolo_data/dataset.yaml
 train_tag=yolov5x_freeze_${freeze_layers}_adam_hypl_decay_lf0005
 
 freeze_layers=18
+epochs = 100
 
 # Train
 
@@ -14,7 +15,7 @@ rm -rf yolov5/runs/train/$train_tag
 python3 yolov5/train.py \
 --img 640 \
 --batch 25 \
---epochs 100  \
+--epochs $epochs  \
 --data $dataset_in_yolo_form_location \
 --weights yolov5x.pt \
 --name $train_tag \
