acdfe91c13c2e0d9ef0812854e2a453c8643dc4f
Kirralex <kirranalex@outlook.com>
2021-10-30 13:50:15 +0300

    Images to bite-code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import yaml
 import cv2
+import requests
 
 app = Flask(__name__)
 
@@ -8,11 +9,19 @@ app = Flask(__name__)
 @app.route('/')
 def main_page():
     # image_classes = list(2)
+    images = {}
 
     with open('images_paths.yml') as f:
-        images_paths = yaml.load(f, Loader=yaml.FullLoader)
+        images_paths = yaml.load(f, Loader=yaml.FullLoader)[0]
+
+    for i in range(len(images_paths)):
+        with open(images_paths['camera{}'.format(i + 1)], 'rb') as file:
+            images['camera{}'.format(i + 1)] = file.read()
+
+    # response = requests.post()
+
     images_classes = ['safe', 'dangerous', 'dangerous', 'safe']
-    
+
     return render_template('main_page.html', images_paths=images_paths, image_classes=images_classes)
 
 
