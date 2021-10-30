028e6b41ea4d729190e0d459de5aaae06e4ec9ea
Anvar <ankhafizov1998@yandex.ru>
2021-10-30 21:35:53 +0300

    fix

--- train_and_make_submit.sh
+++ train_and_make_submit.sh
@@ -1,11 +1,10 @@
 ## Parameters
-test_images_location=/home/akhafizov/Desktop/data_Evraz/test/images
-dataset_in_coco_form_location=yolo_data/dataset.yaml
 
-freeze_layers=18
-
+test_images_location=yolo_data/images/test
+dataset_in_yolo_form_location=yolo_data/dataset.yaml
 train_tag=yolov5x_freeze_${freeze_layers}_adam_hypl_decay_lf0005
 
+freeze_layers=18
 
 # Train
 
@@ -16,7 +15,7 @@ python3 yolov5/train.py \
 --img 640 \
 --batch 25 \
 --epochs 100  \
---data $dataset_in_coco_form_location \
+--data $dataset_in_yolo_form_location \
 --weights yolov5x.pt \
 --name $train_tag \
 --workers 12 \
